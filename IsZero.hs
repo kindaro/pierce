fc034ff9f2d4fbf65ded5153b4b0b137c865c3aa
Ignat Insarov <kindaro@gmail.com>
2018-07-22 14:48:20 +0700

    Evaluate "-3".
    The diff is somewhat longish, so a commentary is in order.
    
    It has become clear that the part of the evaluator that transforms
    numerals to Integer numbers is the inverse of the part of the parser
    that transforms numbers to numerals, so it would be desirable to check
    these particular pieces of functionality against each other. At the same
    time, the partiality in the current implementation has become obvious:
    the logic of rendering numbers is enacted on a subset of L consisting of
    Z, S and P only. So, I decided to extract these constructors to a
    separate type, so that a partial function L -> Integer might become a
    total function N -> Integer that is straightforward to check. The rest
    of the commit is the checking machinery.

--- IsZero.hs
+++ IsZero.hs
@@ -2,6 +2,20 @@ module IsZero where
 
 import Text.ParserCombinators.ReadP
 
+-- $setup
+-- λ import Test.QuickCheck
+-- λ import Test.Invariant
+-- λ import GHC.Generics
+-- λ import Generic.Random
+-- λ :set -XStandaloneDeriving
+-- λ :set -XDeriveGeneric
+-- λ :set -XFlexibleInstances
+-- λ :set -XUndecidableInstances
+-- λ deriving instance Generic L
+-- λ deriving instance Generic N
+-- λ instance Arbitrary N where arbitrary = genericArbitraryU
+-- λ instance Arbitrary L where arbitrary = genericArbitraryU
+
 -- |
 -- λ string "a" $$ "ab"
 -- [("a","b")]
@@ -18,50 +32,54 @@ p $$$ s = fst . head $ readP_to_S (p >>= \x -> eof >> return x) s
 data L = T        -- True.
        | F        -- False.
        | I L L L  -- If.
-       | Z        -- Zero.
-       | S L      -- Successor.
-       | P L      -- Predecessor.
+       | N N      -- Numeral.
        | E L      -- Is zero?
-    deriving Show
+    deriving (Show, Eq)
+
+-- | Numerals.
+data N = Z        -- Zero.
+       | S N      -- Successor.
+       | P N      -- Predecessor.
+    deriving (Show, Eq)
 
 -- | The parser.
 --
 -- λ parser $$ " if true then succ 0 else if iszero succ 0 then 0 else succ 0 "
--- [(I T (S Z) (I (E (S Z)) Z (S Z)),"")]
+-- [(I T (N (S Z)) (I (E (N (S Z))) (N Z) (N (S Z))),"")]
 --
 -- λ parser $$ " if true then (succ 0) else (if (iszero (succ 0)) then 0 else (succ 0)) "
--- [(I T (S Z) (I (E (S Z)) Z (S Z)),"")]
+-- [(I T (N (S Z)) (I (E (N (S Z))) (N Z) (N (S Z))),"")]
 --
 -- λ parser $$ "if true then 1 else if iszero 1 then 0 else 1"
--- [(I T (S Z) (I (E (S Z)) Z (S Z)),"")]
+-- [(I T (N (S Z)) (I (E (N (S Z))) (N Z) (N (S Z))),"")]
 --
 -- λ parser $$ "true"
 -- [(T,"")]
 -- λ parser $$ "false"
 -- [(F,"")]
 -- λ parser $$ "0"
--- [(Z,"")]
+-- [(N Z,"")]
 -- λ parser $$ "succ 0"
--- [(S Z,"")]
+-- [(N (S Z),"")]
 -- λ parser $$ "pred 0"
--- [(P Z,"")]
+-- [(N (P Z),"")]
 -- λ parser $$ "succ pred 0"
--- [(S (P Z),"")]
+-- [(N (S (P Z)),"")]
 -- λ parser $$ "iszero 0"
--- [(E Z,"")]
+-- [(E (N Z),"")]
 -- λ parser $$ "if true then 0 else 0"
--- [(I T Z Z,"")]
+-- [(I T (N Z) (N Z),"")]
 -- λ parser $$ "7"
--- [(S (S (S (S (S (S (S Z)))))),"")]
+-- [(N (S (S (S (S (S (S (S Z))))))),"")]
 -- λ parser $$ "-3"
--- [(P (P (P Z)),"")]
+-- [(N (P (P (P Z))),"")]
 
 parser :: ReadP L
 parser = choice $ fmap whitespaced
                 $                         parsers
                     ++ fmap parenthesized parsers
   where
-    parsers = [ t, f, i, s, p, e, n ]
+    parsers = [ t, f, i, e, n ]
 
     whitespaced = between skipSpaces skipSpaces
     parenthesized = between (string "(") (string ")")
@@ -76,29 +94,67 @@ parser = choice $ fmap whitespaced
         string "else"
         iElse <- parser
         return (I iPred iThen iElse)
+    e = string "iszero" >> E <$> parser
+    n = N <$> parseN
+
+parseN :: ReadP N
+parseN = choice [ s, p, n ]
+  where
     s = do
         string "succ"
-        x <- parser
+        x <- parseN
         return (S x)
     p = do
         string "pred"
-        x <- parser
+        x <- parseN
         return (P x)
-    e = string "iszero" >> E <$> parser
-    n = convert <$> readS_to_P (reads :: ReadS Integer)
-      where convert 0 = Z
-            convert x | x > 0 = S (convert $ pred x)
-                      | x < 0 = P (convert $ succ x)
+    n = toN <$> readS_to_P (reads :: ReadS Integer)
+
+-- |
+-- λ toN 7
+-- S (S (S (S (S (S (S Z))))))
+-- λ toN (-3)
+-- P (P (P Z))
+
+toN :: Integer -> N
+toN 0 = Z
+toN x | x > 0 = S (toN $ pred x)
+            | x < 0 = P (toN $ succ x)
+
+-- |
+-- λ fromN (S (S (S (S (S (S (S Z)))))))
+-- 7
+-- λ fromN (P (P (P Z)))
+-- -3
+--
+-- α fromN `inverts` toN
+--
+-- Notice that to every Integer correspond many N, since `S . P = id`.
+
+fromN :: N -> Integer
+fromN Z = 0
+fromN (S x) = succ (fromN x)
+fromN (P x) = pred (fromN x)
+
+-- |
+-- λ simplify $ S (P (S Z))
+-- S Z
+--
+-- α (fromN . simplify) <=> fromN
+--
+-- α \x -> (toN `inverts` fromN) x == (simplify <=> id) x
+
+simplify :: N -> N
+simplify = toN . fromN
 
 -- |
 -- λ evaluator $ parser $$$ "7"
 -- 7
+-- λ evaluator $ parser $$$ "-3"
+-- -3
 
 evaluator :: L -> Integer
-evaluator x@ (S _) = convert x
-  where convert Z = 0
-        convert (S x) = succ (convert x)
-        convert (P x) = pred (convert x)
+evaluator (N x) = fromN x
 
 -- TODO:
 -- [*] Allow optional parentheses.
